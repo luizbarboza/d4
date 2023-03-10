ec37a59530d9863ae4d8e6139087c08e89260f5d
SBanksX <32441588+SBanksX@users.noreply.github.com>
2023-03-09 23:14:49 -0300

    first commit

---
+++ lib/d4.dart
@@ -0,0 +1,10 @@
+/// D4 is an ongoing project to port the
+/// [D3 JavaScript library](https://github.com/d3/d3) to the Dart language.
+///
+/// **D3** (or **D3.js**) is a JavaScript library for visualizing data using web
+/// standards. D3 helps you bring data to life using SVG, Canvas and HTML. D3
+/// combines powerful visualization and interaction techniques with a
+/// data-driven approach to DOM manipulation, giving you the full capabilities
+/// of modern browsers and the freedom to design the right visual interface for
+/// your data.
+export "src/d4.dart";
---
+++ lib/src/d4.dart
@@ -0,0 +1,2 @@
+export "package:d4_geo/d4_geo.dart";
+export "package:d4_geo_parse/d4_geo_parse.dart";
