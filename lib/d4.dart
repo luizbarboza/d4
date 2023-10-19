9f6353f392d1b54540676f3a0d560b4634fa0031
Luiz Felipe Machado Barboza <32441588+luizbarboza@users.noreply.github.com>
2023-10-19 08:23:55 -0700

    links to added packages

--- lib/d4.dart
+++ lib/d4.dart
@@ -3,9 +3,13 @@
 ///
 /// See one of:
 ///
-/// * [d4_array](https://pub.dev/documentation/d4_array/latest/) - List manipulation, ordering, searching, summarizing, etc.
-/// * [d4_color](https://pub.dev/documentation/d4_color/latest/) - Color spaces! RGB, HSL, Cubehelix, CIELAB, and more.
-/// * [d4_dsv](https://pub.dev/documentation/d4_dsv/latest/) - A parser and formatter for delimiter-separated values, such as CSV and TSV.
-/// * [d4_geo](https://pub.dev/documentation/d4_geo/latest/) - Geographic projections, spherical shapes and spherical trigonometry.
-/// * [d4_interpolate](https://pub.dev/documentation/d4_interpolate/latest/) - Interpolate numbers, colors, strings, arrays, objects, whatever!
+/// * [d4_array](https://pub.dev/packages/d4_array) - List manipulation, ordering, searching, summarizing, etc.
+/// * [d4_color](https://pub.dev/packages/d4_color) - Color spaces! RGB, HSL, Cubehelix, CIELAB, and more.
+/// * [d4_dsv](https://pub.dev/packages/d4_dsv) - A parser and formatter for delimiter-separated values, such as CSV and TSV.
+/// * [d4_format](https://pub.dev/packages/d4_format) - Format numbers for human consumption.
+/// * [d4_geo](https://pub.dev/packages/d4_geo) - Geographic projections, spherical shapes and spherical trigonometry.
+/// * [d4_interpolate](https://pub.dev/packages/d4_interpolate) - Interpolate numbers, colors, strings, lists, maps, whatever!
+/// * [d4_scale](https://pub.dev/packages/d4_scale) - Encodings that map abstract data to visual representation.
+/// * [d4_time](https://pub.dev/packages/d4_time) - A calculator for humanity’s peculiar conventions of time.
+/// * [d4_time_format](https://pub.dev/packages/d4_time_format) - Parse and format times, inspired by strptime and strftime.
 export "src/d4.dart";
